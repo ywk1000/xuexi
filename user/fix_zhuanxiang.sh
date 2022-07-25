d4ba2c1aea7ad50f55f99139056ace620c601531
ywk1000 <22372766+ywk1000@users.noreply.github.com>
2022-07-25 13:37:34 +0800

    Update and rename 1 to fix_zhuanxiang.sh

---
+++ user/fix_zhuanxiang.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#随机数,表示随机一个30以内的数
+#randNum=$(($RANDOM%59))
+
+echo"修复专项"
+cp /xuexi/user/answer_question.py /xuexi/pdlearn/answer_question.py
+sleep 10
+echo"修改web显示"
+cp /xuexi/user/index.html /xuexi/static/index.html
+sleep 10
+echo"修改定时随机延迟"
+cp /xuexi/user/run.sh /xuexi/run.sh
+echo"修改完成"
+exit

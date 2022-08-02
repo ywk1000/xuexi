43e770b3277e3578ec1eb2d4429df141110b301d
ywk1000 <22372766+ywk1000@users.noreply.github.com>
2022-08-02 16:01:30 +0800

    Update run.sh

--- user/run.sh
+++ user/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#随机数,表示随机一个59以内的数
-randNum=$(($RANDOM%59))
+#随机数,表示随机一个3599以内的数
+randNum=$(($RANDOM%3599))
 
 echo "删除缓存文件"
 cd /xuexi/user
 rm user_status.json web_listener.log article_video_index.json daily.log
 
-#随机延迟多少分钟
-sleep randNum*60
+#随机延迟多少秒
+sleep randNum
 
 echo "开始运行"
 /usr/local/bin/python /xuexi/pandalearning.py

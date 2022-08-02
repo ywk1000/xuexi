59696960a18ebe2c75c8a52ee312538b831f3f92
ywk1000 <22372766+ywk1000@users.noreply.github.com>
2022-08-02 16:00:41 +0800

    Update fix_zhuanxiang.sh

--- user/fix_zhuanxiang.sh
+++ user/fix_zhuanxiang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#随机数,表示随机一个30以内的数
-#randNum=$(($RANDOM%59))
+#随机数,表示随机一个3600以内的数
+#randNum=$(($RANDOM%3599))
 
 echo "修复专项"
 cp /xuexi/user/answer_question.py /xuexi/pdlearn/answer_question.py
@@ -10,6 +10,7 @@ cp /xuexi/user/index.html /xuexi/static/index.html
 sleep 3
 echo "修改定时随机延迟"
 cp /xuexi/user/run.sh /xuexi/run.sh
+chmod +x /xuexi/run.sh
 echo "修改完成"
 
 exit
